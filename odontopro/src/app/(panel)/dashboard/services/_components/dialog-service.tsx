1f02ff3713958ad3d416fe1eb86ee4c4cb0d9dde
Felipe Yasuo <felipe.yasuo@hotmail.com>
2026-01-31 21:24:28 -0300

    feat: adiciona máscara de preço no formulário de serviço

--- odontopro/src/app/(panel)/dashboard/services/_components/dialog-service.tsx
+++ odontopro/src/app/(panel)/dashboard/services/_components/dialog-service.tsx
@@ -1,12 +1,10 @@
-"use client";
+"use client"
 
-import {
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-} from "@/components/ui/dialog";
+// - Valor em centavos = Valor em reais * 100
+// - Valor em reais = Valor em centavos / 100
 
-import { useDialogServiceForm } from "./dialog-serivce-form";
+import { DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog"
+import { useDialogServiceForm, DialogServiceFormData } from "./dialog-service-form"
 import {
   Form,
   FormControl,
@@ -15,26 +13,50 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export function DialogService() {
 
   const form = useDialogServiceForm()
 
 
+  async function onSubmit(values: DialogServiceFormData) {
 
+    console.log(values)
+  }
+
+
+  function changeCurrency(event: React.ChangeEvent<HTMLInputElement>) {
+    let { value } = event.target;
+    value = value.replace(/\D/g, '');
+
+    if (value) {
+      value = (parseInt(value, 10) / 100).toFixed(2);
+      value = value.replace('.', ',');
+      value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    }
+
+    event.target.value = value;
+    form.setValue("price", value)
+
+  }
 
   return (
     <>
       <DialogHeader>
         <DialogTitle>Novo Serviço</DialogTitle>
-        <DialogDescription>Adicione um novo serviço</DialogDescription>
+        <DialogDescription>
+          Adicione um novo serviço
+        </DialogDescription>
       </DialogHeader>
 
       <Form {...form}>
-        <form className="space-y-2">
+        <form
+          className="space-y-2"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+
           <div className="flex flex-col">
             <FormField
               control={form.control}
@@ -45,13 +67,16 @@ export function DialogService() {
                     Nome do serviço:
                   </FormLabel>
                   <FormControl>
-                    <Input {...field}
-                      placeholder="Digite o nome do serviço..." />
+                    <Input
+                      {...field}
+                      placeholder="Digite o nome do serviço..."
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
 
             <FormField
               control={form.control}
@@ -62,17 +87,20 @@ export function DialogService() {
                     Valor do serviço:
                   </FormLabel>
                   <FormControl>
-                    <Input {...field}
-                      placeholder="Ex: 120,00" />
+                    <Input
+                      {...field}
+                      placeholder="Ex: 120,00"
+                      onChange={changeCurrency}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-
           </div>
 
-          <p className="font-semibold">Tempo de duração do serviço</p>
+
+          <p className="font-semibold">Tempo de duração do serviço:</p>
           <div className="grid grid-cols-2 gap-3">
             <FormField
               control={form.control}
@@ -83,15 +111,18 @@ export function DialogService() {
                     Horas:
                   </FormLabel>
                   <FormControl>
-                    <Input {...field}
+                    <Input
+                      {...field}
                       placeholder="1"
                       min="0"
-                      type="number" />
+                      type="number"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
             <FormField
               control={form.control}
               name="minutes"
@@ -101,10 +132,12 @@ export function DialogService() {
                     Minutos:
                   </FormLabel>
                   <FormControl>
-                    <Input {...field}
+                    <Input
+                      {...field}
                       placeholder="0"
                       min="0"
-                      type="number" />
+                      type="number"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -112,10 +145,12 @@ export function DialogService() {
             />
           </div>
 
-          <Button type="submit" className="w-full font-semibold text-white"></Button>
+          <Button type="submit" className="w-full font-semibold text-white">
+            Adicionar serviço
+          </Button>
 
         </form>
       </Form>
     </>
-  );
+  )
 }
