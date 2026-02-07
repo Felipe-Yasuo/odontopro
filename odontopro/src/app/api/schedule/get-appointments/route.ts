6700887444353445429e16f0da17c29f3fdf01b2
Felipe Yasuo <felipe.yasuo@hotmail.com>
2026-02-06 23:30:00 -0300

    feat: preparar fluxo de busca de horários agendados por data

--- odontopro/src/app/api/schedule/get-appointments/route.ts
+++ odontopro/src/app/api/schedule/get-appointments/route.ts
@@ -4,12 +4,41 @@ import prisma from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  // Buscar se tem agendamentos (appointments) em uma data especifica de uma clinica.
+  const { searchParams } = request.nextUrl;
 
-  // Quais horarios estão reservados.
+  const userId = searchParams.get('userId')
+  const dateParam = searchParams.get('date')
 
-  return NextResponse.json({
-    ok: true
-  })
+  if (!userId || userId === "null" || !dateParam || dateParam === "null") {
+    return NextResponse.json({
+      error: "Nenhum agendamento encotnrado"
+    }, {
+      status: 400
+    })
+  }
+
+  try {
+    // Converte a data recebida em um objeto Date
+    const [year, month, day] = dateParam.split("-").map(Number)
+    const startDate = new Date(year, month - 1, day, 0, 0, 0)
+    const endDate = new Date(year, month - 1, day, 23, 59, 59, 999)
+
+    console.log("Start date: ", startDate)
+    console.log("End date: ", endDate)
+
+
+    return NextResponse.json({
+      ok: true
+    })
+
+
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({
+      error: "Nenhum agendamento encotnrado"
+    }, {
+      status: 400
+    })
+  }
 
 }
