d67a1525363c117949e5c92bd8735347266067f2
Felipe Yasuo <felipe.yasuo@hotmail.com>
2026-02-08 10:55:14 -0300

    feat: adiciona react query no layout e busca agendamentos por data no dashboard

--- odontopro/src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ odontopro/src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -9,6 +9,8 @@ import {
   CardTitle,
   CardDescription
 } from '@/components/ui/card'
+import { useQuery } from '@tanstack/react-query'
+import { format } from 'date-fns'
 
 interface AppointmentsListProps {
   times: string[]
@@ -19,7 +21,35 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
   const searchParams = useSearchParams();
   const date = searchParams.get("date")
 
-  console.log(date);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-appointments", date],
+    queryFn: async () => {
+
+      let activeDate = date;
+
+      if (!activeDate) {
+        const today = format(new Date(), "yyyy-MM-dd")
+        activeDate = today;
+      }
+
+
+      const url = `${process.env.NEXT_PUBLIC_URL}/api/clinic/appointments?date=${activeDate}`
+
+      const response = await fetch(url)
+
+      const json = await response.json();
+
+      console.log(json);
+
+      if (!response.ok) {
+        return []
+      }
+
+      return json
+
+    }
+  })
 
 
   return (
@@ -42,7 +72,7 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
                 className='flex items-center py-2 border-t last:border-b'
               >
                 <div className='w-16 text-sm font-semibold'>{slot}</div>
-                <div className='flex-1 text-sm text-gray-500'>
+                <div className='flex-1 text-sm'>
                   Disponível
                 </div>
               </div>
