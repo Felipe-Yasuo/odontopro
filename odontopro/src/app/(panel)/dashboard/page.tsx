316b022ecc7610d9f37d076920f560c9c37827e2
Felipe Yasuo <felipe.yasuo@hotmail.com>
2026-02-08 08:07:11 -0300

    adiciona acoes do dashboard e botao de copiar link da clinica

--- odontopro/src/app/(panel)/dashboard/page.tsx
+++ odontopro/src/app/(panel)/dashboard/page.tsx
@@ -1,23 +1,37 @@
-import getSession from '@/lib/getSession'
+import { Button } from '@/components/ui/button'
+import getSesion from '@/lib/getSession'
+import { Calendar } from 'lucide-react'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
+import { ButtonCopyLink } from './_components/button-copy-link'
 
 export default async function Dashboard() {
-  const session = await getSession()
+  const session = await getSesion()
+
 
   if (!session) {
-    redirect('/')
+    redirect("/")
   }
 
   return (
-    <div>
-      <h1>Pagina Dashboard</h1>
+    <main>
+      <div className='space-x-2 flex items-center justify-end'>
+        <Link
+          href={`/clinica/${session.user?.id}`}
+          target='_blank'
+        >
+          <Button className='bg-emerald-500 hover:bg-emerald-400 flex-1 md:flex-[0]'>
+            <Calendar className='w-5 h-5' />
+            <span>Novo agendamento</span>
+          </Button>
+        </Link>
 
-      <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
+        <ButtonCopyLink userId={session.user?.id!} />
+      </div>
 
-      <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
+      <section className='grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4'>
 
-      <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
-    </div>
+      </section>
+    </main>
   )
 }
-
