a9ce09061afafd5bfef6a087abe1e37873a85796
Felipe Yasuo <felipe.yasuo@hotmail.com>
2026-02-08 08:57:33 -0300

    feat: adiciona textarea, modal de novo lembrete e acao de deletar lembrete

--- odontopro/src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
+++ odontopro/src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
@@ -5,6 +5,19 @@ import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card"
 import { Reminder } from "@prisma/client"
 import { Plus, Trash } from "lucide-react"
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { deleteReminder } from '../../_actions/delete-reminder'
+import { toast } from 'sonner'
+import { useRouter } from "next/navigation"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogDescription
+} from '@/components/ui/dialog'
+import { ReminderContent } from './reminder-content'
+
 
 interface ReminderListProps {
   reminder: Reminder[]
@@ -12,7 +25,21 @@ interface ReminderListProps {
 
 export function ReminderList({ reminder }: ReminderListProps) {
 
-  console.log(reminder)
+  const router = useRouter();
+
+  async function handleDeleteReminder(id: string) {
+    const response = await deleteReminder({ reminderId: id })
+
+    if (response.error) {
+      toast.error(response.error)
+      return;
+    }
+
+    toast.success(response.data);
+    router.refresh();
+  }
+
+
 
   return (
     <div className="flex flex-col gap-3">
@@ -22,9 +49,24 @@ export function ReminderList({ reminder }: ReminderListProps) {
             Lembretes
           </CardTitle>
 
-          <Button variant="ghost" className="w-9 p-0">
-            <Plus className="w-5 h-5" />
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="ghost" className="w-9 p-0">
+                <Plus className="w-5 h-5" />
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Novo Lembrete</DialogTitle>
+                <DialogDescription>Criar um novo lembrete para sua lista.</DialogDescription>
+              </DialogHeader>
+
+              <ReminderContent />
+            </DialogContent>
+
+          </Dialog>
+
         </CardHeader>
 
         <CardContent>
@@ -34,21 +76,27 @@ export function ReminderList({ reminder }: ReminderListProps) {
             </p>
           )}
 
-          {reminder.map((item) => (
-            <article
-              key={item.id}
-              className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
-            >
-              <p className="text-sm lg:text-base">{item.description}</p>
-
-              <Button
-                className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p-2"
-                size="sm"
+          <ScrollArea
+            className="h-[340px] lg:max-h-[calc(100vh-15rem)] pr-0 w-full flex-1"
+          >
+            {reminder.map((item) => (
+              <article
+                key={item.id}
+                className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
               >
-                <Trash className="w-4 h-4 text-white" />
-              </Button>
-            </article>
-          ))}
+                <p className="text-sm lg:text-base">{item.description}</p>
+
+                <Button
+                  className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p-2"
+                  size="sm"
+                  onClick={() => handleDeleteReminder(item.id)}
+                >
+                  <Trash className="w-4 h-4 text-white" />
+                </Button>
+              </article>
+            ))}
+          </ScrollArea>
+
         </CardContent>
       </Card>
     </div>
