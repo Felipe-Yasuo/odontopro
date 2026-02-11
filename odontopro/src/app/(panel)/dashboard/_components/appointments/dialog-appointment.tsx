803df1b5413b459130d47faa29ac2c0186a92214
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-02-10 22:36:22 -0300

    feat: adiciona mais detalhes do agendamento no modal (data, cliente e servico)

--- odontopro/src/app/(panel)/dashboard/_components/appointments/dialog-appointment.tsx
+++ odontopro/src/app/(panel)/dashboard/_components/appointments/dialog-appointment.tsx
@@ -5,15 +5,14 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog";
 import { AppointmentWithService } from "./appointments-list";
+import { format } from 'date-fns'
+import { formatCurrency } from '@/utils/formatCurrency'
 
 interface DialogAppointmentProps {
     appointment: AppointmentWithService | null;
 }
 
 export function DialogAppointment({ appointment }: DialogAppointmentProps) {
-
-    console.log("AGENDAMENTO CLICADO: ", appointment);
-
     return (
         <DialogContent>
             <DialogHeader>
@@ -29,8 +28,17 @@ export function DialogAppointment({ appointment }: DialogAppointmentProps) {
 
                 {appointment && (
                     <article>
+                        <p><span className="font-semibold">Horario agendado:</span> {appointment.time}</p>
+                        <p className="mb-2"><span className="font-semibold">Data do agendamento:</span> {format(appointment.appointmentDate, "dd/MM/yyyy")}</p>
                         <p><span className="font-semibold">Nome:</span> {appointment.name}</p>
                         <p><span className="font-semibold">Telefone:</span> {appointment.phone}</p>
+                        <p><span className="font-semibold">Email:</span> {appointment.email}</p>
+
+                        <section className="bg-gray-100 mt-4 p-2 rounded-md">
+                            <p><span className="font-semibold">Serviço:</span> {appointment.service.name}</p>
+                            <p><span className="font-semibold">Valor:</span> {formatCurrency((appointment.service.price / 100))}</p>
+                        </section>
+
                     </article>
                 )}
 
