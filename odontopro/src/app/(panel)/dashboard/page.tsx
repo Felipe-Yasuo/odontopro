889d6c64eb3dc68eafd402b5a2f9c2b16eebbf09
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-02-12 22:07:02 -0300

    feat: adiciona verificação de trial e bloqueio do dashboard quando assinatura expir

--- odontopro/src/app/(panel)/dashboard/page.tsx
+++ odontopro/src/app/(panel)/dashboard/page.tsx
@@ -6,14 +6,17 @@ import { redirect } from 'next/navigation'
 import { ButtonCopyLink } from './_components/button-copy-link'
 import { Reminders } from './_components/reminder/reminders'
 import { Appointments } from './_components/appointments/appointments'
+import { checkSubscription } from '@/utils/permissions/checkSubscription'
+import { LabelSubscription } from '@/components/ui/label-subscription'
 
 export default async function Dashboard() {
   const session = await getSesion()
 
-
   if (!session) {
     redirect("/")
   }
+
+  const subscription = await checkSubscription(session?.user?.id!)
 
   return (
     <main>
@@ -22,7 +25,7 @@ export default async function Dashboard() {
           href={`/clinica/${session.user?.id}`}
           target='_blank'
         >
-          <Button className='bg-emerald-500 hover:bg-emerald-400 flex-1 md:flex-0'>
+          <Button className='bg-emerald-500 hover:bg-emerald-400 flex-1 md:flex-[0]'>
             <Calendar className='w-5 h-5' />
             <span>Novo agendamento</span>
           </Button>
@@ -31,11 +34,26 @@ export default async function Dashboard() {
         <ButtonCopyLink userId={session.user?.id!} />
       </div>
 
-      <section className='grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4'>
-        <Appointments userId={session.user?.id!} />
+      {subscription?.subscriptionStatus === "EXPIRED" && (
+        <LabelSubscription expired={true} />
+      )}
 
-        <Reminders userId={session.user?.id!} />
-      </section>
+      {subscription?.subscriptionStatus === "TRIAL" && (
+        <div className='bg-green-500 text-white text-sm md:text-base px-3 py-2 rounded-md my-2'>
+          <p className='font-semibold'>
+            {subscription?.message}
+          </p>
+        </div>
+      )}
+
+      {subscription?.subscriptionStatus !== "EXPIRED" && (
+        <section className='grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4'>
+          <Appointments userId={session.user?.id!} />
+
+          <Reminders userId={session.user?.id!} />
+        </section>
+      )}
+
     </main>
   )
 }
