80e5822d2dacc896c06a570e46fb4ee1d5cfeb74
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-02-13 12:36:53 -0300

    feat: adiciona login com google usando authjs e libera imagem do google no next

--- odontopro/src/app/(public)/_components/header.tsx
+++ odontopro/src/app/(public)/_components/header.tsx
@@ -1,34 +1,30 @@
 "use client"
 
-import { useState } from "react";
+import { useState } from 'react'
 import Link from "next/link";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import { Button } from "@/components/ui/button";
+import { Button } from "../../../components/ui/button";
 import { LogIn, Menu } from "lucide-react";
-import { useSession } from "next-auth/react";
-import { handleRegister } from "../_actions/login";
+import { useSession } from 'next-auth/react'
+import { handleRegister } from '../_actions/login'
 
 export function Header() {
-
   const { data: session, status } = useSession();
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const navItems = [
     { href: "#profissionais", label: "Profissionais" },
   ]
 
-
   async function handleLogin() {
-    await handleRegister("github")
+    await handleRegister("google")
   }
 
   const NavLinks = () => (
@@ -38,22 +34,23 @@ export function Header() {
           onClick={() => setIsOpen(false)}
           key={item.href}
           asChild
-          className="bg-transparent hover:bg-transparent text-black shadow-none">
-          <Link href={item.href} className="text-base">
+          className="bg-transparent hover:bg-transparent text-black shadow-none"
+        >
+          <Link href={item.href} className='text-base'>
             {item.label}
           </Link>
         </Button>
       ))}
 
-      {status === "loading" ? (
+      {status === 'loading' ? (
         <></>
       ) : session ? (
         <Link
           href="/dashboard"
-          className="flex items-center justify-center gap-2 bg-zinc-900 text-white py-1 rounded-md px-4">
-          Acessar Clinica
+          className='flex items-center justify-center gap-2 bg-zinc-900 text-white py-1 rounded-md px-4'
+        >
+          Acessar clinica
         </Link>
-
       ) : (
         <Button onClick={handleLogin}>
           <LogIn />
@@ -63,18 +60,21 @@ export function Header() {
     </>
   )
 
+
+
   return (
     <header
-      className="fixed top-0 right-0 left-0 z-999 py-6 bg-white">
+      className="fixed top-0 right-0 left-0 z-999 py-4 px-6 bg-white"
+    >
       <div className="container mx-auto flex items-center justify-between">
         <Link
           href="/"
           className="text-3xl font-bold text-zinc-900"
         >
-          Odonto<span className="text-emerald-500">Pro</span>
+          Odonto<span className="text-emerald-500">PRO</span>
         </Link>
 
-        <nav className="hidden md:flex items-cente space-x-4">
+        <nav className="hidden md:flex items-center space-x-4">
           <NavLinks />
         </nav>
 
@@ -92,18 +92,18 @@ export function Header() {
           <SheetContent side="right" className="w-[240px] sm:w-[300px] z-9999">
             <SheetTitle>Menu</SheetTitle>
             <SheetHeader></SheetHeader>
+
             <SheetDescription>
-              Veja Nossos Links
+              Veja nossos links
             </SheetDescription>
 
-            <nav className="flex flex-col space-y-4 mt-6">
+            <nav className='flex flex-col space-y-4 mt-6'>
               <NavLinks />
             </nav>
 
           </SheetContent>
+
         </Sheet>
-
-
       </div>
     </header>
   )
